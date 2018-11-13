ce95770b7f59588ec2bd240d4451dd720365aa73
Mitesh Malaviya <miteshmalaviya8@gmail.com>
2018-11-12 22:45:05 -0500

    final done

--- firstFile.py
+++ firstFile.py
@@ -5,23 +5,12 @@ MUSHROOM_SWISS = 5.95
 WESTERN_BURGER = 5.95
 DON_CALI_BURGER = 5.95
 TAX = 1.09
-QUANTITY1 = 0
-QUANTITY2 = 0
-QUANTITY3 = 0
-QUANTITY4 = 0
-QUANTITY5 = 0
-TOTAL1 = 0
-TOTAL2 = 0
-TOTAL3 = 0
-TOTAL4 = 0
-TOTAL5 = 0
+
 
 
 def main():
     displayMenu()
-    orderTotal = takeOrder()
-    taxRate = studentOrStaff()
-    orderTotalAfterTax = (calculateTotalAfter(orderTotal, taxRate))
+    takeOrder()
 
 
 def displayMenu():
@@ -33,53 +22,64 @@ def displayMenu():
 
 
 def takeOrder():
-    while(True):
+
+    QUANTITY1 = 0
+    QUANTITY2 = 0
+    QUANTITY3 = 0
+    QUANTITY4 = 0
+    QUANTITY5 = 0
+    TOTAL1 = 0
+    TOTAL2 = 0
+    TOTAL3 = 0
+    TOTAL4 = 0
+    TOTAL5 = 0
+
+    userInputFlag = True
+    while(userInputFlag):
 
         order = int(input("Which burger would you like? "))
-
         if(order == 0):
+            print("")
             print("Thanks!")
-            break
+            userInputFlag = False
         elif(order > 5 or order < 0):
+            print("")
             print("Please Select a valid option")
-            break
+            userInputFlag = False
 
-        quantity = int(input("How many would you like? "))
+        if(userInputFlag == True):
+            quantity = int(input("How many would you like? "))
+            if(order == 1):
+                TOTAL1 = quantity * DE_ANZA_BURGER
+                QUANTITY1 += 1
+                quantity_ = QUANTITY1
+                QUANTITY1 = QUANTITY1 * quantity
+                continue
+            elif(order == 2):
+                TOTAL2 = quantity * BACON_CHEESE
+                QUANTITY2 += 1
+                QUANTITY2 = QUANTITY2 * quantity
+                continue
+            elif(order == 3):
+                TOTAL3 = quantity * MUSHROOM_SWISS
+                QUANTITY3 += 1
+                QUANTITY3 = QUANTITY3 * quantity
+                continue
+            elif(order == 4):
+                TOTAL4 = quantity * WESTERN_BURGER
+                QUANTITY4 += 1
+                QUANTITY4 = QUANTITY4 * quantity
+                continue
+            elif(order == 5):
+                TOTAL5 = quantity * DON_CALI_BURGER
+                QUANTITY5 += 1
+                QUANTITY5 = QUANTITY5 * quantity
+                continue
 
-        if order == 1:
-            TOTAL1 = quantity * DE_ANZA_BURGER
-            QUANTITY1 += 1
-            quantity_ = QUANTITY1
-            QUANTITY1 = QUANTITY1 * quantity
-            continue
-        elif order == 2:
-            TOTAL2 = quantity * BACON_CHEESE
-            QUANTITY2 += 1
-            QUANTITY2 = QUANTITY2 * quantity
-            continue
-        elif order == 3:
-            TOTAL3 = quantity * MUSHROOM_SWISS
-            QUANTITY3 += 1
-            QUANTITY3 = QUANTITY3 * quantity
-            continue
-        elif order == 4:
-            TOTAL4 = quantity * WESTERN_BURGER
-            QUANTITY4 += 1
-            QUANTITY4 = QUANTITY4 * quantity
-            continue
-        elif order == 5:
-            TOTAL5 = quantity * DON_CALI_BURGER
-            QUANTITY5 += 1
-            QUANTITY5 = QUANTITY5 * quantity
-            continue
-
-
-    print(QUANTITY1, QUANTITY2, QUANTITY3, QUANTITY4, QUANTITY5)
-    print(TOTAL1, TOTAL2, TOTAL3, TOTAL4, TOTAL5)
-    grandTotal = TOTAL1 + TOTAL2 + TOTAL3 + TOTAL4 + TOTAL5
-    print(grandTotal)
-
-    return (grandTotal)
+    totalBeforeTax = TOTAL1 + TOTAL2 + TOTAL3 + TOTAL4 + TOTAL5
+    taxRate = studentOrStaff()
+    totalAfterTax = calculateTotalAfter(totalBeforeTax, taxRate)
+    printBill(QUANTITY1, TOTAL1, QUANTITY2, TOTAL2, QUANTITY3, TOTAL3, QUANTITY4, TOTAL4, QUANTITY5, TOTAL5, totalBeforeTax, taxRate, totalAfterTax)
 
 
 def calculateTotalAfter(totalAmount, taxRate):
@@ -87,14 +87,33 @@ def calculateTotalAfter(totalAmount, taxRate):
 
 
 def studentOrStaff():
-
     studentOrStaff = input("Are you a student or staff??: ")
     if studentOrStaff == "staff":
         tax = TAX
     elif studentOrStaff == "student":
         tax = 1
-
+    print("")
     return (tax)
+
+
+def printBill(q1,p1,q2,p2,q3,p3,q4,p4,q5,p5,totalBeforeTax,Tax,totalAfterTax):
+    print("You Ordered: ")
+    print(str(q1) + " DeAnza Burger," + " $" + str(p1))
+    print(str(q2) + " Bacon Cheese," +  " $" + str(p2))
+    print(str(q3) + " Mushroom Swiss," + " $" + str(p3))
+    print(str(q4) + " Western Burger," + " $" + str(p4))
+    print(str(q5) + " Don Cali," + " $" + str(p5))
+    print("")
+    print("Cost Breakdown: ")
+    print("Total Before Tax: " + "$" + str(totalBeforeTax))
+    if(Tax == 1):
+        print("Tax Rate: " + "No Tax")
+    else:
+        print("Tax Rate: " + "%" + str(Tax))
+
+    print("Total Before Tax: " + "$" + str(totalBeforeTax))
+    print("Total After Tax: " + "$" + str(totalAfterTax))
+
 
 
 main()
